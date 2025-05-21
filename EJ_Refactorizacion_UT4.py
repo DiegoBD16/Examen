8b43fa3510cbfd1f28c473c0657f9699487a8b67
Diego Barco Dávila <Diegobarcodavila@gmail.com>
2025-05-21 17:48:29 +0200

    Paso 5, hago un bucle en la funcion principal con todas las funciones del programa y cambio la funcion crear_receta de sitio

--- EJ_Refactorizacion_UT4.py
+++ EJ_Refactorizacion_UT4.py
@@ -44,23 +44,47 @@ class utilidades:
         for ingrediente in ingredientes:
             print(f"* {ingrediente}")
 
-    @staticmethod
-    def crear_receta(tipo_de_receta):
-        recetas = []
-        if receta == 1:
-            receta = receta_Vegetariana()
-        if receta == 2:
-            receta = receta_No_Vegetariana()
-        recetas.append(receta)
-
 # Función principal
 def principal():
-    #Variables necesarias para la funcion crear_receta
-    nombre_receta = ""
+    pregunta = 0
+    recetas = []
+    listas_de_ingredientes = []
+    while pregunta != 4:
+        pregunta = int(input("¿Que quieres hacer? 1: crear receta, 2: mostrar receta, 3: mostrar ingredientes, 4: finalizar"))
+        if pregunta == 1:
+            crear_receta(recetas, listas_de_ingredientes)
+        elif pregunta == 2:
+            receta = int(input("Que receta quieres imprimir?: "))
+            utilidades.imprimir_receta(recetas[receta])
+        elif pregunta == 3:
+            receta = int(input("¿De que receta quieres los ingredientes?: "))
+            utilidades.mostrar_lista_ingredientes(listas_de_ingredientes[receta])
+
+def crear_receta(recetas, listas_de_ingredientes):
     ingredientes_receta = []
-    ingredientes_pasos = []
+    receta_pasos = []
+    tipo_de_receta = int(input("¿Escribe 1 para una receta vegetariana o 2 para una receta no vegetariana: "))
+    nombre_receta = str(input("¿Nombre?: "))
 
+            #Pido los ingredientes
+    numero_ingredientes = int(input("¿Cuantos ingredientes llevara?: "))
+    for i in range(numero_ingredientes):
+        ingrediente = str(input("¿Ingrediente?: "))
+        ingredientes_receta.append(ingrediente)
 
+            #Pido los pasos
+    numero_pasos = int(input("¿en cuantos pasos se hace?: "))
+    for p in range(numero_pasos):
+        paso = str(input("¿Paso?: "))
+        receta_pasos.append(paso)
+            #Creo la receta y la guardo en la lista "recetas", lo mismo con los ingredientes que los guardo en "lista_de_ingredientes"
+
+    if tipo_de_receta == 1:
+        receta = receta_Vegetariana(nombre_receta, ingredientes_receta, receta_pasos)
+    elif tipo_de_receta == 2:
+        receta = receta_No_Vegetariana(nombre_receta, ingredientes_receta, receta_pasos)
+    listas_de_ingredientes.append(ingredientes_receta)
+    recetas.append(receta)
 
 # Ejecutar el programa
 if __name__ == "__main__":
